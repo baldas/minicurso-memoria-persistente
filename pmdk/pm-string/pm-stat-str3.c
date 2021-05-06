90cb382130433af2b845459c1fb74341bdf160fa
b4ld4s <alexandro.baldassin@unesp.br>
2021-05-05 21:39:13 -0300

    Adding examples from pmdk repository

---
+++ pmdk/pm-string/pm-stat-str3.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <libpmemobj.h>                                                              
+
+/*
+ Create the pool for this app with:
+ pmempool create obj --layout=strstatic <pool-name>
+*/
+
+#define LAYOUT_NAME "strstatic"
+#define MAX_BUF_LEN 20
+
+struct my_root {
+  char buf[MAX_BUF_LEN];
+};
+
+
+  
+int main(int argc, char *argv[])
+{
+  if (argc != 2) {
+    printf("usage: %s file-name\n", argv[0]);
+    return 1;
+  }
+
+  PMEMobjpool *pop = pmemobj_open(argv[1], LAYOUT_NAME);
+  if (pop == NULL) {
+    perror("pmemobj_open");
+    return 1;
+  }
+
+  PMEMoid root = pmemobj_root(pop, sizeof(struct my_root));
+  struct my_root *rootp = pmemobj_direct(root);
+
+	printf("Last string: %s\n", rootp->buf);
+
+  char buf[MAX_BUF_LEN] = {0};
+  if (scanf("%99[0-9a-zA-Z ]", buf) == EOF) {
+    fprintf(stderr, "EOF\n");
+    return 1;
+  }
+
+  TX_BEGIN(pop) {
+  
+		pmemobj_tx_add_range(root, 0, strlen(buf)+1);
+    memcpy(rootp->buf, buf, strlen(buf)+1);
+
+  } TX_END
+
+
+  pmemobj_close(pop);
+
+  return 0;
+}
